4233c0fd17b91a4bc5639cedf20843359377ad91
Damian F <damian.fikier@wp.pl>
2020-11-30 22:24:26 +0100

    Basic server init with database connection

---
+++ server/ClinicSystem/Database/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ClinicSystem.Database
+{
+    public partial class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public int AppointmentDoctorId { get; set; }
+        public int AppointmentPatientId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+
+        public virtual Account AppointmentDoctor { get; set; }
+        public virtual Account AppointmentPatient { get; set; }
+    }
+}
